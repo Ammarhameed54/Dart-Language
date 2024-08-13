ab7f9d9a9f8170e8b42e9dfbd1c4bbc15e93eff2
Ammar Hameed <f21bdats1m02054@iub.edu.pk>
2024-08-13 04:33:10 -0700

    little more on String

--- Types in Dart/string_in_dart/bin/string_in_dart.dart
+++ Types in Dart/string_in_dart/bin/string_in_dart.dart
@@ -35,4 +35,52 @@ void main(List<String> arguments) {
       r"Hello My Name is Ammar Hameed and i am 21 years old , Currently am a student of Bs Data Science at Islamia University of Bahawalpur";
   print(rawString);
   // if i try to add new line it will give the error
+
+  // String Methods and Properties
+
+  // length method
+  print(multiLineString.length);
+
+  // extract some part of string by using index
+
+  String sub = multiLineString.substring(20, 50);
+  print(sub);
+
+  // (contain) check if certain character or word is present in string or not
+  print(multiLineString.contains("Hello")); //false
+  print(multiLineString.contains("is")); // true
+
+  // Replace something is string
+
+  String text = "Hello Dart";
+  String replaced = text.replaceAll("Dart", "Flutter");
+  print(replaced);
+
+  // (Trim) to remove the leading and trailing whitespaces
+
+  String word = "    Hello  Flutetr         ";
+  print(word.trim());
+
+  // lower case and Uppercase
+
+  print(text.toLowerCase());
+  print(text.toUpperCase());
+
+  // Some Escape Sequences
+  /* \n : Newline
+  \t : Tab
+  \\ : Backslash
+  \' : Single quote
+  \" : Double quote
+
+*/
+
+// Unicodes
+// we can use unicode for any character or for any emoji or direct print emoji
+
+  print("💙");
+  String heart = "💙";
+
+  // runes for unicodes
+  print(heart.runes);
 }
